2b3e2fd1d64a3e61a0255b01572639bfed8bb025
Noah <noahrules96950@gmail.com>
2020-06-05 04:44:17 +1000

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/NewBanking.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewBanking.dir/Banking.cpp.obj"
+  "CMakeFiles/NewBanking.dir/main.cpp.obj"
+  "NewBanking.exe"
+  "NewBanking.exe.manifest"
+  "NewBanking.pdb"
+  "libNewBanking.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewBanking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
